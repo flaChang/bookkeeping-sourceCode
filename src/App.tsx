18f37add89d6b8342755cfac9779e22b74e49c5d
flaChang <zza46585@163.com>
2021-12-16 23:48:42 +0800

    初始化init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
 
+
+const Button = styled.button`
+  color: red;
+`
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div><Button>nihao1</Button></div>
+    );
 }
 
 export default App;
