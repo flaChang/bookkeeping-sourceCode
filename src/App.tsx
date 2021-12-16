5cc434570bc69ecbdbba12f219e186091949e799
flaChang <zza46585@163.com>
2021-12-16 23:52:28 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import styled from 'styled-components'
-import './App.css';
-
-
-const Button = styled.button`
-  color: red;
-`
 
 function App() {
     return (
-        <div><Button>nihao1</Button></div>
+        <div>hi  zza</div>
     );
 }
 
