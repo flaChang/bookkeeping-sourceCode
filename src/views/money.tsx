c2306e649537d2190fd691cb05c62025efc94638
flaChang <zza46585@163.com>
2021-12-19 17:37:55 +0800

    money.tsx 之 numpadSection

--- src/views/money.tsx
+++ src/views/money.tsx
@@ -43,7 +43,25 @@ const CategorySection = styled.section`
   }
 `
 const NumberPadSection = styled.section`
-
+   display: flex;flex-direction: column;
+   > .output{
+     background: white;font-size: 36px;
+     line-height: 72px;text-align: right;padding: 0 16px;
+     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                 inset 0 5px 5px -5px rgba(0,0,0,0.25);
+   }
+   > .pad{
+     > button {font-size: 18px;float: left;width: 25%;height: 64px;
+       &.ok{height: 128px;float: right;}
+       &.dot{width: 50%;}
+       &:nth-child(1){background: red;}
+       &:nth-child(2),&:nth-child(5){background: green;}
+       &:nth-child(3),&:nth-child(6),&:nth-child(9){background: red;}
+       &:nth-child(4),&:nth-child(7),&:nth-child(10){}
+       &:nth-child(8),&:nth-child(11),&:nth-child(13){}
+       &:nth-child(12),&:nth-child(14){}
+     }
+   }
 `
 
 function Money() {
@@ -71,23 +89,25 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberPadSection>
-                <div>
+                <div className="output">
                     100
                 </div>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>ok</button>
-                <button>0</button>
-                <button>.</button>
+                <div className="pad clearfix">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button className="ok">ok</button>
+                    <button  className='dot'>0</button>
+                    <button>.</button>
+                </div>
             </NumberPadSection>
         </Layout>
     )
