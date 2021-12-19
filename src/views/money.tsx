f68b505c582dc667e9121845dab57a9eac5e9884
flaChang <zza46585@163.com>
2021-12-19 16:57:01 +0800

    money.tsx 之 categorySection

--- src/views/money.tsx
+++ src/views/money.tsx
@@ -31,7 +31,16 @@ const NotesSection = styled.section`
     }
 `
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul { display: flex;background: #c4c4c4;
+    > li {
+      width: 50%;text-align: center;padding: 16px 0;position: relative;
+      &.selected::after{
+        content: '';display: block;width: 100%;left: 0;bottom: 0;
+        height: 3px;background: #333;position: absolute;
+      }
+    }
+  }
 `
 const NumberPadSection = styled.section`
 
@@ -57,7 +66,7 @@ function Money() {
             </NotesSection>
             <CategorySection>
                 <ul>
-                    <li>支出</li>
+                    <li className='selected'>支出</li>
                     <li>收入</li>
                 </ul>
             </CategorySection>
