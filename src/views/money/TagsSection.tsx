b2d1712316cda9e956cddb1564c2d3b04f2053fb
flaChang <zza46585@163.com>
2021-12-19 20:52:23 +0800

    tagsection 选中及新增

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #ffffff;padding: 12px 16px;flex-grow: 1;
   display: flex;flex-direction: column;justify-content: flex-end;align-items: flex-start;
   > ol {margin: 0 -12px;
@@ -9,6 +10,9 @@ const TagsSection = styled.section`
       background: #D9D9D9;border-radius: 18px;
       display: inline-block;padding: 3px 18px;
       font-size: 14px;margin: 8px 12px;
+      &.selected{
+        background: #000;
+      }
     }
   }
 
@@ -17,5 +21,37 @@ const TagsSection = styled.section`
     border-bottom: 1px solid #333;color: #666;margin-top: 10px;
   }
 `
+
+const TagsSection: React.FC = (props) => {
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
+    const [selectedTags,setSelectedTags]=useState<string[]>([])
+    const onAddTag =()=>{
+        const tagName= window.prompt('新标签的名称为');
+        if (tagName!==null){
+            setTags([...tags,tagName])
+        }
+    }
+    const onToggleTag=(tag:string)=>{
+        const index = selectedTags.indexOf(tag);
+        if(index>=0){
+            setSelectedTags(selectedTags.filter(t=>t!==tag)); //若tag已被选中，复制所有没被选中的tag作为新的selectedTags
+        }else{
+            setSelectedTags([...selectedTags,tag])
+        }
+    }
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag=>
+                    <li key={tag} onClick={
+                        ()=>{onToggleTag(tag)}
+                    } className={selectedTags.indexOf(tag)>=0 ? 'selected':''}>
+                        {tag}
+                    </li>)}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    )
+}
 
 export {TagsSection};
