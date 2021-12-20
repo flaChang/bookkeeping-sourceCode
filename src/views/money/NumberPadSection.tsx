55ed3fd65fe982fcea11b55b805bdab7141b3f13
flaChang <zza46585@163.com>
2021-12-20 11:37:46 +0800

    数字板分隔成组件

--- src/views/money/NumberPadSection.tsx
+++ src/views/money/NumberPadSection.tsx
@@ -1,27 +1,6 @@
-import styled from "styled-components";
 import React, {useState} from "react";
-
-const Wrapper = styled.section`
-   display: flex;flex-direction: column;
-   > .output{
-     background: white;font-size: 36px;
-     line-height: 72px;text-align: right;padding: 0 16px;
-     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                 inset 0 5px 5px -5px rgba(0,0,0,0.25);
-   }
-   > .pad{
-     > button {font-size: 18px;float: left;width: 25%;height: 64px;border: none;
-       &.ok{height: 128px;float: right;}
-       &.dot{width: 50%;}
-       &:nth-child(1){background: red;}
-       &:nth-child(2),&:nth-child(5){background: green;}
-       &:nth-child(3),&:nth-child(6),&:nth-child(9){background: red;}
-       &:nth-child(4),&:nth-child(7),&:nth-child(10){}
-       &:nth-child(8),&:nth-child(11),&:nth-child(13){}
-       &:nth-child(12),&:nth-child(14){}
-     }
-   }
-`
+import {Wrapper} from "./NumberPadSection/Wrapper";
+import {getIntext} from "./NumberPadSection/getIntext";
 
 const NumberPadSection:React.FC=()=>{
     const [output,_setOutput] =useState<string>('0')
@@ -36,40 +15,12 @@ const NumberPadSection:React.FC=()=>{
     const onClickFather = (e:React.MouseEvent)=>{
         const inText=(e.target as HTMLButtonElement).textContent;
         if(inText===null){return}
-        switch(inText){
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case '0':
-                if(output==="0"){
-                    setOutput(inText)
-                }else{
-                    setOutput(output+inText)
-                }
-                break;
-            case '.':
-                if(output.indexOf('.')>=0){return;}
-                setOutput(output+'.')
-                break;
-            case '删除':
-                if(output.length===1){
-                    setOutput('')
-                }else{
-                    setOutput(output.slice(0,-1))
-                }
-                break;
-            case '清空':
-                setOutput('0')
-                break;
-            case 'OK':
-
-                break;
+        if(inText==="OK"){
+            //todo
+            return;
+        }
+        if('0123456789.'.split('').concat(['删除','清空']).indexOf(inText)>=0){
+            setOutput( getIntext(inText,output))
         }
 
     }
---
+++ src/views/money/NumberPadSection/getIntext.tsx
@@ -0,0 +1,34 @@
+const getIntext=(inText:string,output='0')=>{
+    switch(inText){
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            if(output==="0"){
+                return inText
+            }else{
+                return output+inText
+            }
+        case '.':
+            if(output.indexOf('.')>=0){return output;}
+            return output+'.'
+        case '删除':
+            if(output.length===1){
+                return ''
+            }else{
+                return output.slice(0,-1)
+            }
+        case '清空':
+            return ''
+        default:
+            return ''
+    }
+}
+
+export {getIntext}
